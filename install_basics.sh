122f594dd8acf798d37285607e5825250a9b1314
AwesomeTech-Inc <74341317+AwesomeTech-Inc@users.noreply.github.com>
2020-11-12 14:25:34 +0900

    Add files via upload

---
+++ install_basics.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+
+set -e
+
+if ! grep 'cuda/bin' ${HOME}/.bashrc > /dev/null ; then
+  echo "** Add CUDA stuffs into ~/.bashrc"
+  echo >> ${HOME}/.bashrc
+  echo "export PATH=/usr/local/cuda/bin\${PATH:+:\${PATH}}" >> ${HOME}/.bashrc
+  echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64\${LD_LIBRARY_PATH:+:\${LD_LIBRARY_PATH}}" >> ${HOME}/.bashrc
+fi
+
+sudo apt-get install python-pip python3-pip
+
+mkdir -p ${HOME}/tensorRT
+git clone https://github.com/jkjung-avt/tensorrt_demos ${HOME}/tensorRT
+
+reboot
